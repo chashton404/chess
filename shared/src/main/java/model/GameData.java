7264b864738c2e3b1cef199a8690a5d1321ee5d9
Chase Ashton <chashton404@gmail.com>
2026-02-28 22:12:26 -0700

    created data model classes

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,12 @@
+package model;
+
+import chess.ChessGame;
+import com.google.gson.*;
+
+public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+    
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+
+}
