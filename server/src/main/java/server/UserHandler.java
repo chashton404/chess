256a2b0d62227ba7901eee95d0e997f95b863ea1
Chase Ashton <chashton404@gmail.com>
2026-03-05 10:17:15 -0700

    Implementation of Register endpoint on Handler

--- server/src/main/java/server/UserHandler.java
+++ server/src/main/java/server/UserHandler.java
@@ -1,11 +1,15 @@
 package server;
 
+import dataaccess.AlreadyTakenException;
+import dataaccess.BadRequestException;
 import io.javalin.http.Context;
 import service.AuthService;
 import service.UserService;
 
 import model.RegisterRequest;
 import model.RegisterResult;
+
+import java.util.Map;
 
 public class UserHandler {
 
@@ -17,7 +21,7 @@ public class UserHandler {
         this.userService = userService;
     }
 
-    public void UserHandler(Context ctx){
+    public void registerUser(Context ctx){
         try {
             // This using features of Javalin and GSON to take the body of the HTTP request (JSON) and turn it into a RegsiterRequest
             // In order for this to work the field names in RegisterRequest need to match the JSON keys in the body exactly
@@ -29,10 +33,15 @@ public class UserHandler {
             // Now we return the write HTTP code and the body
             ctx.status(200);
             ctx.json(res);
+        } catch (BadRequestException e) {
+            ctx.status(400);
+            ctx.json(Map.of("message", e.getMessage()));
+        } catch (AlreadyTakenException e) {
+            ctx.status(403);
+            ctx.json(Map.of("message", e.getMessage()));
+        } catch (Exception e) {
+            ctx.status(500);
+            ctx.json(Map.of("message", e.getMessage()));
         }
-        
-
     }
-
-    
 }
