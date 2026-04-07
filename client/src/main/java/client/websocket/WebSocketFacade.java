2097d4bca4ddd7ba22468ac7342756ec19a63b76
Chase Ashton <chashton404@gmail.com>
2026-04-06 19:14:03 -0600

    Commented out notify for now

--- client/src/main/java/client/websocket/WebSocketFacade.java
+++ client/src/main/java/client/websocket/WebSocketFacade.java
@@ -35,7 +35,7 @@ public class WebSocketFacade extends Endpoint {
                 public void onMessage(String message) {
                     // When we receive a message these are the things that should be done
                     ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
-                    notificationHandler.notify(serverMessage);
+                    // notificationHandler.notify(serverMessage);
                 }
             });
         } catch (DeploymentException | IOException | URISyntaxException ex) {
